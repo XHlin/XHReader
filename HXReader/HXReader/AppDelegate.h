00250bd43353938d4353a9646606bb90695e075b
小黑 <2508204001@qq.com>
2016-09-22 12:34:43 +0800

    文档规范

---
+++ HXReader/HXReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXReader
+//
+//  Created by 小黑 on 16/9/22.
+//  Copyright © 2016年 小黑. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
